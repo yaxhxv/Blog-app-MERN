aeb073e2f65f018707ca1796bd22f69885aaa7e6
yaxh.xv <yashnawgiri45@gmail.com>
2024-10-14 08:33:24 +0530

    debugged the mongoose schema

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,12 +7,19 @@ exports.getAllUsers = (req, res, next) => {
   };
   
   //create user register user
-  exports.registerController = async (req, res, next) => {
+  exports.registerController = async (req, res) => {
    try {
-    const  {username : userName, email, password } = req.body;
+    console.log(req.body)
+    const  {userName, email, password } = req.body;
     //validation
-    if(!userName  || !email || !password) {
-        return res.status(400).json({ message: 'Please enter all fields' });
+    if(!userName ) {
+        return res.status(400).json({ message: 'Please enter userName' });
+    }
+    if( !email ) {
+        return res.status(400).json({ message: 'Please enter email' });
+    }
+    if( !password) {
+        return res.status(400).json({ message: 'Please enter password' });
     }
 
     const existingUser  = await userModel.findOne({ email });
@@ -21,7 +28,7 @@ exports.getAllUsers = (req, res, next) => {
     }
     
     //save new user
-    const newUser =  new  userModel({username,email,password})
+    const newUser =  new  userModel({userName,email,password})
     await  newUser.save();
     return  res.status(201).json({ message: 'User created successfully',
         sucess:true,
