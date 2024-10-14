8aa32cfe0a07e3125b0b3db0516bb48ad6e2604f
yaxh.xv <yashnawgiri45@gmail.com>
2024-10-14 09:22:08 +0530

    updated the userController file

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,21 @@
 const userModel = require('../models/userModel')
+const bcrypt = require('bcrypt')
 
 
 //get all users
-exports.getAllUsers = (req, res, next) => {
-    // your implementation here
+exports.getAllUsers = async (req, res) => {
+    try {
+      const users = await userModel.find({});
+      return res.status(200).send({
+        userCount: users.length,
+        sucess:true,
+        message:"all users data",
+        users
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(500)
+    }
   };
   
   //create user register user
@@ -26,9 +38,11 @@ exports.getAllUsers = (req, res, next) => {
     if(existingUser){
         return res.status(400).json({ message: 'Email already in use' });
     }
+
+    const hashedPassword = await bcrypt.hash(password,10)
     
     //save new user
-    const newUser =  new  userModel({userName,email,password})
+    const newUser =  new  userModel({userName,email,password: hashedPassword})
     await  newUser.save();
     return  res.status(201).json({ message: 'User created successfully',
         sucess:true,
@@ -47,6 +61,52 @@ exports.getAllUsers = (req, res, next) => {
   };
   
   // login 
-  exports.loginController = (req, res, next) => {
-    // your implementation here
+  exports.loginController =async (req, res, next) => {
+      try {
+        const  { email, password } = req.body;
+        if(!email) {
+          return res.status(401).send({
+            success:false,
+            message:"please provide email"
+          })
+        }
+        if(!password) {
+          return res.status(401).send({
+            success:false,
+            message:"please provide password"
+          })
+        }
+
+
+        const user = await userModel.findOne({email})
+        if(!user){
+          return res.status(200).send({
+            sucess:false,
+            message:"email is not registered"
+          })
+        }
+// password
+
+        const isMatch = await bcrypt.compare(password, user.password )
+        if(!isMatch){
+          return res.status(401).send({
+            sucess:false,
+            message:'invalid username or password'
+          })
+        }
+
+        return res.status(200).send({
+          sucess:true,
+          message:"login sucessfully",
+          user
+        })
+
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+          success:false,
+          message: "Error in loginController",
+          error: error.message
+        })
+      }
   };
